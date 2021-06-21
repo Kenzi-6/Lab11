4d4a9a6f47aa80ece57ed3aaa8e6007a5eefdaf3
Kenzi-6 <77572112+Kenzi-6@users.noreply.github.com>
2021-06-21 13:19:47 +0300

    Add files via upload

---
+++ main7.java
@@ -0,0 +1,43 @@
+package com.company;
+import java.io.*;
+public class main7 {
+    public static void main(String[] args) {
+        rd();
+    }
+    public static void rd(){
+        try(BufferedReader read = new BufferedReader(new FileReader("TextWithComments.txt"))){
+            String cod;
+            boolean prov = false;
+            while ((cod = read.readLine()) != null){
+                boolean result = cod.matches("^(//.*)|(.*/\\*.*)|(.*\\*/)$");
+                if(result){
+                    boolean AnResult = cod.matches("^(.*\\*/)$");
+                    boolean AnResult_2 = cod.matches("^(.*/\\*.*)$");
+                    if(AnResult){
+                        prov = false;
+                    }
+                    else if (AnResult_2){
+                        prov = true;
+                    }
+                    else{
+                        prov = false;
+                    }
+                }
+                else if (!prov){
+                    wr(cod);
+                }
+            }
+        }
+        catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    public static void wr(String cod){
+        try(BufferedWriter wr = new BufferedWriter(new FileWriter("TextWithoutComments.txt", true))){
+            wr.write(cod + "\n");
+        }
+        catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
